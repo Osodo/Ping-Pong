39f760b23fb922705ca38503f76e0b404db3283b
“George <grgosodo@gmail.com>
2025-07-09 06:15:52 +0300

    Please enter the commit message for your changes. Lines starting with '#' will be ignored, and an empty message aborts the commit.
    On branch main
    
    Initial commit
    
    Changes to be committed:
    	new file:   main.py

---
+++ main.py
@@ -0,0 +1,18 @@
+from turtle import Screen
+
+
+screen = Screen()
+screen.setup(width = 800, height = 600)
+screen.title("PingPong by Osodo")
+screen.bgcolor("black")
+
+
+
+
+
+
+
+
+
+
+screen.exitonclick()
