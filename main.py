d870bffed8c359f81a3ccf3b7b12b04201b1bc2d
“George <grgosodo@gmail.com>
2025-07-09 21:23:11 +0300

    ggg

--- main.py
+++ main.py
@@ -7,11 +7,16 @@ screen.setup(width = 800, height = 600)
 screen.title("PingPong by Osodo")
 screen.bgcolor("black")
 screen.tracer(0)
-right_paddle = Paddle()
+r_paddle = Paddle((350,0))
+l_paddle = Paddle((-350,0))
 
 screen.listen()
-screen.onkeypress(key="Up", fun = right_paddle.go_up)
-screen.onkeypress(key="Down", fun = right_paddle.go_down)
+screen.onkeypress(key="Up", fun = r_paddle.go_up)
+screen.onkeypress(key="Down", fun = r_paddle.go_down)
+
+screen.onkeypress(key="w", fun = l_paddle.go_up)
+screen.onkeypress(key="s", fun = l_paddle.go_down)
+
 
 game_is_on = True
 while game_is_on:
--- paddle.py
+++ paddle.py
@@ -1,17 +1,19 @@
 from turtle import Turtle
 
 class Paddle(Turtle):
-    def __init__(self):
-        self.paddle = Turtle( shape="square")
-        self.paddle.penup()
-        self.paddle.goto(x=350, y=0)
-        self.paddle.color("white")
-        self.paddle.shapesize(stretch_wid=5.0, stretch_len=1.0)
+
+    def __init__(self, position):
+        super().__init__()
+        self.shape("square")
+        self.penup()
+        self.goto(position)
+        self.color("white")
+        self.shapesize(stretch_wid=5.0, stretch_len=1.0)
 
     def go_up(self):
-        new_y = self.paddle.ycor() + 20
-        self.paddle.goto(self.paddle.xcor(), new_y)
+        new_y = self.ycor() + 20
+        self.goto(self.xcor(), new_y)
 
     def go_down(self):
-        new_y = self.paddle.ycor() - 20
-        self.paddle.goto(self.paddle.xcor(), new_y)
+        new_y = self.ycor() - 20
+        self.goto(self.xcor(), new_y)
